67b83d4cec7e93f8f6533f768c6f4d9603477bbc
Gonzalo Alcina <gonzalo.alcinabenavente@gmail.com>
2024-12-12 16:55:22 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+import Test from "./src/scripts/test.js";
+
+class Main {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    new Test();
+  }
+}
+
+new Main();
---
+++ src/scripts/test.js
@@ -0,0 +1,11 @@
+class Test {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    console.log("working");
+  }
+}
+
+export default Test;
